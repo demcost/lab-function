87840b01943313a0707afce02489c8691929b918
Demetrio Costa <demcost@microsoft.com>
2026-08-19 14:37:45 -0300

    Add .NET 8 Azure Function with HTTP trigger and GitHub Actions CI/CD workflow

---
+++ HttpTriggerFunction.cs
@@ -0,0 +1,23 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab_function;
+
+public class HttpTriggerFunction
+{
+    private readonly ILogger<HttpTriggerFunction> _logger;
+
+    public HttpTriggerFunction(ILogger<HttpTriggerFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HttpTriggerFunction")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
